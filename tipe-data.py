54baae84871cd960314274b1a60c0ddfdd89e83d
Azfer Jausyaq Fauzi <mangvidch@gmail.com>
2025-07-20 20:30:53 +0700

    Create tipe-data.py
    Belajar tipe data dasar pada python

---
+++ tipe-data.py
@@ -0,0 +1,27 @@
+# Pengenalan tipe data di Bahasa Pemograman Python 
+# Yang umum diketahui ada 4 tipe data, yaitu :
+
+# INTEGER
+# Integer atau biasa di singkat menjadi (int) hanya terdiri dari bilangan bulat saja, contoh 1,2,3,dan 4
+tipe_data_int = 3
+print(tipe_data_int)
+print(type(tipe_data_int))
+
+# FLOAT
+# Float atau biasa di tulis (float) adalah tipe data yang hanya terdiri dari bilangan decimal, contoh 3.14 dan 0.5 (jika di python penulisannya tidak menggunakan koma ',' tetapi menggunakan tititk '.')
+tipe_data_float = 2.25
+print(tipe_data_float)
+print(type(tipe_data_float))
+
+# STRING
+# String atau biasa di singkat (str) adalah tipe data yang berupa karakter dengan menggunakan kutipan seperti '' atau ""
+tipe_data_str = "Hello World"
+print(tipe_data_str)
+print(type(tipe_data_str))
+
+# BOOLEAN
+# Boolean atau biasa disingkat (bool) adalah sebuah tipe data yang hanya berisi False atau True
+tipe_data_bool = True
+print(tipe_data_bool)
+print(type(tipe_data_bool))
+
