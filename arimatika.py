83f6aa74e54a6bcbc0ead9f7b68b51fa6f52763c
Azfer Jausyaq Fauzi <mangvidch@gmail.com>
2025-08-02 21:10:25 +0700

    Create arimatika.py
    Belajar Proses arimatika di Python

---
+++ arimatika.py
@@ -0,0 +1,65 @@
+# PROSES ARITMATIKA 
+a = 10 
+b = 2
+
+# Pertambahan (+)
+x = b + a
+print(x)
+
+# Pergurangan (-)
+x = b - a
+print(x)
+
+# Perkalian (*)
+x = b * a
+print(x)
+
+# Pembagian (/)
+x = b / a
+print(x)
+
+# Perpangkatan (**)
+x = b ** a
+print(x)
+
+# Modulus (%), menyisakan hasil pembagian
+x = b % a
+print(x)
+
+# Floor (//), menggenapkan hasil pembagian
+x = b // a
+print(x)
+
+# Urutang perhitungan proses aritmatika ini adalah :
+
+# - ( ), dalam kurung 
+# - **, exponen
+# - perkalian dan pembagian
+# - pertambahan dan pengurangan
+
+# Contoh :
+
+x = a + b - a * (b / a) ** a % b // a
+print(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
