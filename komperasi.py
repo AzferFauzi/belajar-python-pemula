3573efc6cb4ed6b166194ede4b3b391453d72cd1
Azfer Jausyaq Fauzi <mangvidch@gmail.com>
2025-08-02 21:26:45 +0700

    Create komperasi.py
    Operasi Komperasi di Python

---
+++ komperasi.py
@@ -0,0 +1,50 @@
+# OPERASI KOMPERASI
+# Adalah sebuah proses yang akan mengeluarkan hasil tipe data boolean yaitu False atau True
+
+a = 10
+b = 5
+
+# Operasi >
+x = a > b
+print(f"Apakah a > b = {x}")
+
+# Operasi <
+x = a < b
+print(f"Apakah a < b = {x}")
+
+# Operasi >=
+x = a >= b
+print(f"Apakah a >+ b = {x}")
+
+# Operasi <=
+x = a <= b
+print(f"Apakah a <= b = {x}")
+
+# Operasi ==
+x = a == b
+print(f"Apakah a == b = {x}")
+
+# Operasi !=
+x = a != b
+print(f"Apakah a != b = {x}")
+
+
+# Operasi Komperasi is dan is not
+# Operasi ini sama seperti == dan !=
+# yang membedakannya adalah ketika variable yang di bandingkan adalah bilangan secara langsung
+# akan tetap sama hasilnya, tapi kurang cocok untuk Operasi ini
+
+x = a is b
+print(f"Apakah a adalah b = {x}")
+
+x = a is not b
+print(f"Apakah a atidak b = {x}")
+
+
+
+
+
+
+
+
+
