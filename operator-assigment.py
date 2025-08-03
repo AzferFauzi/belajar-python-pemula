10226fc90a25d77ab0ef2451a08f23c6570fb459
Azfer Jausyaq Fauzi <mangvidch@gmail.com>
2025-08-03 18:44:26 +0700

    Create operator-assigment.py
    Belajar Operator Assigment dan Bonus Operator Bitwise di Python

---
+++ operator-assigment.py
@@ -0,0 +1,65 @@
+# OPERATOR ASSIGMENT
+# Mempersingkat perhitungan
+
+a = 10
+
+# Pertambahan
+a += 1
+print(f"a + 1 = {a}")
+# Pengurangan
+a -= 5
+print(f"a - 5 = {a}")
+# Perkalian
+a *= 3
+print(f"a * 3 = {a}")
+# Pembagian
+a /= 2
+print(f"a / 2 = {a}\n")
+
+a = 3
+# Perpangkatan
+a **= 4
+print(f"a ** 4 = {a}")
+# Permodulusan
+a %= 4
+print(f"a % 4 = {a}")
+# Perflooran
+a //= 4
+print(f"a // 4 = {a}\n")
+
+# Operasi Logika Assigment
+
+a = True
+b = False
+# AND
+print("==== AND ====")
+a &= True
+print(f"True and True = {a}")
+a &= False
+print(f"True and False = {a}")
+b &= True
+print(f"Fales and True = {a}")
+b &= False
+print(f"False and False = {a}\n")
+
+# OR
+print("==== OR ====")
+a |= True
+print(f"True or True = {a}")
+a |= False
+print(f"True or False = {a}")
+b |= True
+print(f"Fales or True = {a}")
+b |= False
+print(f"False or False = {a}\n")
+
+# XOR
+print("==== XOR ====")
+a ^= True
+print(f"True xor True = {a}")
+a ^= False
+print(f"True xor False = {a}")
+b ^= True
+print(f"Fales xor True = {a}")
+b ^= False
+print(f"False xor False = {a}\n")
