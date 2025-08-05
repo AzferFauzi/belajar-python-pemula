483a7f6299fba4930f40b7771ecfce9c73efa0d2
Azfer Jausyaq Fauzi <mangvidch@gmail.com>
2025-08-05 21:09:52 +0700

    Create if-elif.py
    Belajar IF dan ELSE STATMENT di Python

---
+++ if-elif.py
@@ -0,0 +1,9 @@
+# IF DAN ELSE STATMENT
+# Belajar if dan else 
+
+data = input("Apakaha kamu cinta aku? (ya/tidak) = ")
+
+if data == "ya":
+    print("Ahhh jadi sayang dehhh, Muachhhh")
+else: 
+    print("Keciwoooo")
