c85893ec115337f6fb3064994a2df6cc364acf0c
Azfer Jausyaq Fauzi <mangvidch@gmail.com>
2025-08-25 10:03:24 +0700

    Create loop-while.py
    Belajar Loop While di Python

---
+++ loop-while.py
@@ -0,0 +1,19 @@
+# LOOPING MENGGUNAKAN WHILE DI PYTHON
+# Berbeda dengan loop for in, whilw menggunakan kondisi untuk nge loop  
+
+# Contoh
+data = 0
+
+while data < 10:
+    print("alamak")
+    data += 1
+
+# membuat segitiga menggunakan loop while
+data = 1
+while data < 5:
+    print("*"*data)
+    data +=1
+
+# membuat loop input
+while True:
+    data = input("Apa warna favoritmu = ")
