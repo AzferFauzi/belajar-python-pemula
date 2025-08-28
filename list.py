4f94a4c347abeeaaabf44e8d35016d80f98278cc
Azfer Jausyaq Fauzi <mangvidch@gmail.com>
2025-08-28 10:40:59 +0700

    Create list.py
    Belajar List di Python

---
+++ list.py
@@ -0,0 +1,41 @@
+# LIST
+# Mempelajari list di Python
+
+# Jenis - jenis list di Python
+
+# List Integer
+# berisi kumpulan data integer di dalam list 
+# contoh
+list_int = [1,2,3,4,5]
+print(list_int)
+
+# List String
+# berisi kumpulan data string di dalam list 
+# contoh
+list_str = ["Ayah","Ibu","Kakak","Adik"]
+print(list_str)
+
+# List Boolean
+# berisi kumpulan data Boolean di dalam list 
+# contoh
+list_bool = [True,False,True,False]
+print(list_bool)
+
+# List Campuran
+# Berisi kumpulan berbagai data yang di campur di dalam list
+list_camp = ["Kakak",17,True]
+print(list_camp)
+
+# List comprehension
+list_for = [f"Ganjil {i}" if i % 2 == 1 else f"Genap {i}" for i in range(1,16)]
+print(list_for)
+
+
+
+
+
+
+
+
+
+
