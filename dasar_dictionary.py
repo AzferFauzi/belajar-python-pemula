3448ff59d13ab2e6875bc64594bd813d35b8543c
Azfer Jausyaq Fauzi <mangvidch@gmail.com>
2025-09-08 15:00:36 +0700

    Update dasar_dictionary.py
    Belajar Dasar Dictionary di python

--- dasar_dictionary.py
+++ dasar_dictionary.py
@@ -1,6 +1,8 @@
 # BELAJAR PENGENLAN DICTIONARY
 # dictionary adalah sebuah tipe data yang non immutamble
 
+# cara membuat tipe data dict yaitu dengan cara : data{key:valeu} 
+
 # contoh
 data_dict = {
     1:'Ahmad',
