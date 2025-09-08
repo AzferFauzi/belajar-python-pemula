2055472d6aed057ec5bbe861b0db51a1bd4ef802
Azfer Jausyaq Fauzi <mangvidch@gmail.com>
2025-09-08 14:57:37 +0700

    Create dasar_dictionary.py
    Belajar Tipe Data Dictionary di Python

---
+++ dasar_dictionary.py
@@ -0,0 +1,17 @@
+# BELAJAR PENGENLAN DICTIONARY
+# dictionary adalah sebuah tipe data yang non immutamble
+
+# contoh
+data_dict = {
+    1:'Ahmad',
+    2:'Amara',
+    3:'Ananda',
+    4:'Audrey',
+    5:'Azfer'
+}
+
+print(f'Data Dict ke 1 yaitu = {data_dict[1]}')
+print(f'Data Dict ke 2 yaitu = {data_dict[2]}')
+print(f'Data Dict ke 3 yaitu = {data_dict[3]}')
+print(f'Data Dict ke 4 yaitu = {data_dict[4]}')
+print(f'Data Dict ke 5 yaitu = {data_dict[5]}')
