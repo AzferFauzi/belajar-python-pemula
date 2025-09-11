0f2580bce8aba271722346c386eeedb8ed84e8c5
Azfer Jausyaq Fauzi <mangvidch@gmail.com>
2025-09-11 10:19:27 +0700

    Create data_tuple_set.py
    Belajar Data Set dan Tuple di Python

---
+++ data_tuple_set.py
@@ -0,0 +1,12 @@
+# TUPLE dan SET
+
+# TUPLE
+# Tuple adalah data yang tidak bisa di ubah teapi bisa di ambil 
+data_tuple = (1,2,4,1,3)
+print(f'data_tuple = {data_tuple}')
+print(f'data index ke-1 = {data_tuple[1]}\n')
+
+# SET
+# SET adalah data yang bisa di ubah tapi tidak bisa di ambil karena data nya tidak berbasis index
+data_set = {1,4,3,2,5}
+print(f'data_set = {data_set}')
