b5c78f34df99ba03613ca6514b0f11be6e5f5099
Azfer Jausyaq Fauzi <mangvidch@gmail.com>
2025-10-15 11:26:22 +0700

    Create import.py
    Belajar Fungsi Cara Kerja Import di Python

---
+++ import.py
@@ -0,0 +1,15 @@
+# IMPORT STATMENT
+# Berfungsi untuk mengambil dan menggunakan kode atau fungsi dari modul lain
+
+# contoh 1
+from ___py___ import main1
+print(main1.data)
+
+# contoh 2
+from ___py___ import main2
+print(f'Pesan dari file main2 = {main2.data}')
+
+# contoh 3 
+# jika file fungsi
+from ___py___ import kalkulator
+print(f"Hasil dari 12 + 3 = {kalkulator.tambah(12,3)}")
