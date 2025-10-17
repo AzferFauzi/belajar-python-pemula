a8c25e201cde867cc3b617bfb49f5ff96fb4b971
Azfer Jausyaq Fauzi <mangvidch@gmail.com>
2025-10-17 08:15:11 +0700

    Create module.py
    Belajar Cara Kerja Module di Python

---
+++ module.py
@@ -0,0 +1,9 @@
+# MODULE
+
+from ___py___.kalkulator import kurang,kali,bagi
+from ___py___.kalkulator import tambah as add
+
+print(f"Hasil dari 1 + 7 = {add(1,7)}")
+print(f"Hasil dari 49 - 10 = {kurang(49,10)}")
+print(f"Hasil dari 25 * 4 = {kali(25,4)}")
+print(f"Hasil dari 60 / 5 = {bagi(60,5)}")
